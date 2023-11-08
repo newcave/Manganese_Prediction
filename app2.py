a2760cbc9e5e8e2eb2dc3a902b25a1f594307b64
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-11-08 10:35:01 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
